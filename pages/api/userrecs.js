739e17286e41a55427af039d1ea1948966f294c9
JJPolarbear <75065075+jjpolarbear@users.noreply.github.com>
2022-05-18 03:02:16 -0700

    Add API for adding recommendations to a user

---
+++ pages/api/userrecs.js
@@ -0,0 +1,39 @@
+import clientPromise from '../../lib/mongodb'
+
+export default async function handler(req, res) {
+  const client = await clientPromise
+  await client.connect()
+  const db = client.db('classify')
+  let body
+  let user
+  switch (req.method) {
+    case 'POST':
+      body = JSON.parse(req.body).data
+      user = await db
+        .collection('users')
+        .findOneAndUpdate(
+          { username: body.username },
+          { $addToSet: { recommendations: body.recommendation } }
+        )
+      if (!user) {
+        return res.status(401).json({
+          error: `invalid username`,
+        })
+      }
+      res.status(200).json()
+      break
+    case 'GET':
+      body = JSON.parse(req.body).data
+      user = await db
+        .collection('users')
+        .find({ username: body.username })
+        .next()
+      if (!user) {
+        return res.status(401).json({
+          error: `invalid username`,
+        })
+      }
+      res.status(200).json(user.recommendations)
+      break
+  }
+}
