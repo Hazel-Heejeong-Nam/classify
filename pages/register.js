115699971aba84e80fe3751997aa3e6acdfe05b7
Hazel Heejeong Nam <100391059+Hazel-Heejeong-Nam@users.noreply.github.com>
2022-05-19 01:09:05 -0700

    Update register.js

--- pages/register.js
+++ pages/register.js
@@ -23,15 +23,58 @@ export default function Register() {
     'Anthropology (B.A.)',
     'Anthropology (B.S.)',
     'Arabic (B.A.)',
+    'Asian American Studies (B.A.)',
+    'Asian Humanities (B.A.)',
+    'Asian Languages and Linguistics (B.A.)',
+    'Asian Religions (B.A)',
+    'Asian Studies (B.A.) ',
+    'Business Economics (B.A.) ',
+    'Central and East European Languages and Cultures (B.A.)',
+    'Chicana and Chicano Studies (B.A.)',
+    'Chinese (B.A.)',
+    'Classical Civilization (B.A.)',
+    'Communication (B.A.) ',
+    'Comparative Literature (B.A.)',
+    'Ecology, Behavior, and Evolution (B.S.) ',
+    'Economics (B.A.) ',
+    'English (B.A.)',
+    'European Language and Transcultural Studies (B.A.)',
+    'European Languages and Transcultural Studies with French and Francophone (B.A.)',
+    'European Languages and Transcultural Studies with German (B.A.)',
+    'European Languages and Transcultural Studies with Italian (B.A.)',
+    'European Languages and Transcultural Studies with Scandinavian (B.A.)',
+    'European Studies (B.A.) ',
+    'Gender Studies (B.A.)',
+    'Geography (B.A.)',
+    'Geography/Environmental Studies (B.A.)',
   ]
 
   const AAMajors = [
     'Architectural Studies (B.A.)',
-    'Art (B.A.)',
+    'Art History (B.A.)',
     'Dance (B.A.)',
+
+
   ]
 
-  const possibleMajors = LSMajors.concat(AAMajors)
+  const STEMMajors = [
+    'Astrophysics (B.S.)',
+    'Atmospheric and Oceanic Sciences (B.S.)',
+    'Atmospheric and Oceanic Sciences/Mathematics (B.S.)',
+    'Biochemistry (B.S.)',
+    'Biology (B.S.)',
+    'Biophysics (B.S.)',
+    'Chemistry (B.S.)',
+    'Chemistry/Materials Science (B.S.)',
+    'Climate Science (B.S.) ',
+    'Cognitive Science (B.S.) ',
+    'Computational and Systems Biology (B.S.)',
+    'Earth and Environmental Science (B.A.)',
+    'Environmental Science (B.S.)',
+  ]
+
+  const aa = LSMajors.concat(AAMajors)
+  const possibleMajors = aa.concat(STEMMajors)
   const possibleYears = ['Freshmen', 'Sophomore', 'Junior', 'Senior']
 
   // TODO: redirect to main page if already logged in
