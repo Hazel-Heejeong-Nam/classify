34c35ab1906de41c33a50466ebb8b0c926396a3d
Hazel Heejeong Nam <100391059+Hazel-Heejeong-Nam@users.noreply.github.com>
2022-05-19 01:09:57 -0700

    Update generate.js

--- pages/generate.js
+++ pages/generate.js
@@ -1,8 +1,17 @@
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import FormInput from '../components/FormInput'
+import { useForm } from 'react-hook-form'
+import { Slider, Button, Card, Container, Grid, Stack } from '@mui/material'
+import FormAutocomplete from '../components/FormAutocomplete'
 
 export default function Generate() {
+  const majorclassnum = ['1','2','3','4']
+  const geclassnum = ['1','2','3','4']
+  const { control, handleSubmit } = useForm()
   return (
     <div>
       <Head>
@@ -11,7 +20,60 @@ export default function Generate() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Navbar />
-      <main></main>
+      <main>
+        <Container>
+          <Grid
+            container
+            direction='column'
+            alignItems='center'
+            justifyContent='center'
+          >
+          </Grid>
+          <Card sx={{ minWidth: '350px', padding: '30px', m: '30px' }}>
+            <p>1. Choose how many major classes you want to take next quarter.</p>
+              <Stack spacing={3}></Stack>
+                <FormAutocomplete
+                    name='majorclassnum'
+                    control={control}
+                    label='Choose number'
+                    options={majorclassnum}
+                  />
+            <p>2. Choose how many GE classes you want to take next quarter.</p>
+              <Stack spacing={3}></Stack>
+                <FormAutocomplete
+                  name='geclassnum'
+                  control={control}
+                  label='Choose number'
+                  options={geclassnum}
+                />
+            <p>3. Please check an expectation of your work load.</p>
+              <Stack spacing={3}></Stack>
+                <Slider
+                  aria-label="Work Load"
+                  defaultValue={50}
+                  valueLabelDisplay="auto"
+                  step={10}
+                  marks
+                  min={10}
+                  max={100}
+                />
+            <p>3. Please check an expectation of easiness.</p>
+              <Stack spacing={3}></Stack>
+                <Slider
+                  aria-label="Easiness"
+                  defaultValue={50}
+                  valueLabelDisplay="auto"
+                  step={10}
+                  marks
+                  min={10}
+                  max={100}
+                />    
+          </Card>
+          <Button variant='outlined' type='submit'>
+            SAVE
+          </Button>         
+        </Container>
+      </main>
       <Footer />
     </div>
   )
