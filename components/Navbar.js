82d8209f4b3322b1788ed5d7649a33e39e75ca4e
Hazel Heejeong Nam <100391059+Hazel-Heejeong-Nam@users.noreply.github.com>
2022-05-21 16:57:20 -0700

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,17 @@
-import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material'
+import { IconButton, Container, AppBar, Box, Button, Toolbar, Typography } from '@mui/material'
 import { UserContext } from '../contexts/UserContext'
 import { useContext } from 'react'
 import Link from 'next/link'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 
-
 const ucla = createTheme({
   palette: {
     uclablue: {
       main: '#1a64db',
-      contrastText: '#ffe438'
+      contrastText: '#c99906'
     },
     uclayellow: {
-      main: '#ffe438',
+      main: '#c99906',
       contrastText: '#1a64db'
     },
   },
@@ -42,52 +41,63 @@ const Navbar = () => {
   return (
     <ThemeProvider theme={ucla}>
     <AppBar position='static'>
-      <Toolbar>
-        <Link href='/'>Classify</Link>
-        {user && (
-          <Box sx={{ mr: '0', ml: 'auto' }}>
-            {userPages.map((page) => (
+      <Container maxWidth = "xl">
+        <Toolbar disableGutters>
+          <Box
+            component = "img"
+            sx={{
+              height: 40,
+              width: 40,
+            }}
+            src="logo.png"
+          />
+          <Link href='/'>Classify</Link>
+          {user && (
+            <Box sx={{ mr: '0', ml: 'auto' }}>
+              {userPages.map((page) => (
+                <Button
+                  sx={{ mr: '5px', display: 'inline-block' }}
+                  key={page}
+                  variant='contained'
+                  color= "uclayellow"
+                  href={`/${userPageLinks[page.replaceAll(' ', '_')]}`}
+                >
+                  {page}
+                </Button>
+              ))}
               <Button
                 sx={{ mr: '5px', display: 'inline-block' }}
-                key={page}
-                variant='contained'
-                color= "uclayellow"
-                href={`/${userPageLinks[page.replaceAll(' ', '_')]}`}
-              >
-                {page}
-              </Button>
-            ))}
-            <Button
-              sx={{ mr: '5px', display: 'inline-block' }}
-              key='logout'
-              variant='outlined'
-              color='inherit'
-              onClick={handleLogout}
-            >
-              LOG OUT
-            </Button>
-          </Box>
-        )}
-        {!user && (
-          <Box sx={{ mr: '0', ml: 'auto' }}>
-            {guestPages.map((page) => (
-              <Button
-                sx={{ mr: '5px', display: 'inline-block' }}
-                key={page}
+                key='logout'
                 variant='outlined'
                 color='inherit'
-                href={`${page.toLowerCase()}`}
+                onClick={handleLogout}
               >
-                {page}
+                LOG OUT
               </Button>
-            ))}
-          </Box>
-        )}
-        <Typography>{user?.username ?? 'Guest Bruin'}</Typography>
-      </Toolbar>
+            </Box>
+          )}
+          {!user && (
+            <Box sx={{ mr: '0', ml: 'auto' }}>
+              {guestPages.map((page) => (
+                <Button
+                  sx={{ mr: '5px', display: 'inline-block' }}
+                  key={page}
+                  variant='outlined'
+                  color='inherit'
+                  href={`${page.toLowerCase()}`}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+          )}
+          <Typography>{user?.username ?? 'Guest Bruin'}</Typography>
+        </Toolbar>
+      </Container>
     </AppBar>
     </ThemeProvider>
   )
 }
 
 export default Navbar
+
