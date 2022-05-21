8c8d76ba5b4d83686178b2f704eb15d977690f9f
Hazel Heejeong Nam <100391059+Hazel-Heejeong-Nam@users.noreply.github.com>
2022-05-21 16:58:51 -0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import { Box, Card, Typography } from '@mui/material'
 
 const Footer = () => {
   return (
-    <footer style={{ height: '40px' }}>
+    <footer style={{ height: '50px' }}>
       <Box>
         <Card
           square
@@ -11,10 +11,12 @@ const Footer = () => {
             height: '40px',
             p: 1,
             textAlign: 'center',
-            background: '#1976d2',
+            background: '#000000',
+            border: 3,
+            borderColor :'#c99906'
           }}
         >
-          <Typography color='white'>Footer</Typography>
+          <Typography color='white'>UCLA 2022 Spring ECE188 | CLASSify</Typography>
         </Card>
       </Box>
     </footer>
