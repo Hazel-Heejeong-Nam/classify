cb3b2ecb6b1106faf6823754f51a532a18864d98
JJPolarbear <75065075+jjpolarbear@users.noreply.github.com>
2022-06-01 16:27:34 -0700

    Make app name appear clickable

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,8 +12,6 @@ import { useContext } from 'react'
 import Link from 'next/link'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 
-
-
 const ucla = createTheme({
   palette: {
     uclablue: {
@@ -73,17 +71,15 @@ const Navbar = () => {
               }}
               src='https://postfiles.pstatic.net/MjAyMjA2MDJfMjUg/MDAxNjU0MTIyMzk5OTE4.8OqxEH2E7RcVc9cqLofMmmtLGC6cbJye5P4Omomjkkog.jQnXa5TCK-s_mxNl5wlFtyyh4Omj2QxX4KlQzCJP138g.PNG.hatbi2000/image.png?type=w773'
             />
-            <Link href='/'>
-              <Box
-                sx={{
-                  fontWeight: 'bold',
-                  fontSize: 33,
-                  margin: 2,
-                }}
-              >
-                CLASSify
-              </Box>
-            </Link>
+            <Typography
+              sx={{
+                fontWeight: 'bold',
+                fontSize: 33,
+                ml: 2,
+              }}
+            >
+              <Link href='/'>CLASSify</Link>
+            </Typography>
             {user && (
               <Box sx={{ mr: '0', ml: 'auto' }}>
                 {userPages.map((page) => (
