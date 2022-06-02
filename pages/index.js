28cba18528c24aff012e21558821034f47f32452
JJPolarbear <75065075+jjpolarbear@users.noreply.github.com>
2022-06-02 01:22:55 -0700

    Move recommendation from home page to its proper place
    - Recommendations will now be automatically fetched

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,20 @@
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import { Button, Card, Container, Grid, Stack } from '@mui/material'
-import { useForm } from 'react-hook-form'
+import { Container, Grid, Stack } from '@mui/material'
 import { UserContext } from '../contexts/UserContext'
-import { useContext, useState } from 'react'
+import { useContext } from 'react'
 import { Box } from '@mui/system'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
-var recom = null
 export default function Home() {
   const { user } = useContext(UserContext)
-  const [recommendation, setRecommendation] = useState(null)
-  async function getClasses() {
-    const res = await fetch('http://localhost:3000/api/classes', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }).then((res) => res.json())
-    console.log(res)
-    recom = res
-  }
-  async function getRecs() {
-    const res = await fetch(
-      `http://localhost:3000/api/userrecs?username=${user.username}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    )
-      .then((res) => res.json())
-      .then((rec) => setRecommendation(Object.keys(rec)[0]))
-    alert('Result now available!')
-  }
+  let router = useRouter()
+
+  useEffect(() => {
+    if (user) router.push('/recommendations')
+  })
 
   return (
     <div>
@@ -53,52 +33,21 @@ export default function Home() {
             justifyContent='center'
           ></Grid>
           <Stack spacing={3}>
-            {user && !recommendation && (
-              <Container>
-                <Grid
-                  container
-                  direction='column'
-                  alignItems='center'
-                  justifyContent='center'
-                >
-                  <p>
-                    Click 'Get' and wait. I will let you know when the result is
-                    ready!
-                  </p>
-                  <Box
-                    component='img'
-                    sx={{
-                      height: 380,
-                      width: 540,
-                    }}
-                    src='welcome.jpg'
-                  />
-                  <Button variant='contained' type='submit' onClick={getRecs}>
-                    GET RECOMMENDATION
-                  </Button>
-                </Grid>
-              </Container>
-            )}
-            {user && recommendation && (
-              <p>Here is your recommendation: {recommendation}</p>
-            )}
-            {!user && (
-              <Grid
-                container
-                direction='column'
-                alignItems='center'
-                justifyContent='center'
-              >
-                <Box
-                  component='img'
-                  sx={{
-                    height: 600,
-                    width: 800,
-                  }}
-                  src='https://postfiles.pstatic.net/MjAyMjA2MDJfODMg/MDAxNjU0MTIyMjcwNzE3.2fThIQuTaZl2OOT3uba6gGl6gd-N_JCWGtpJXlqYDIAg.N-WoprcIWBYp_Q_BT1sC1uOEiwYw9EiqbSLIHNR3x-Ug.JPEG.hatbi2000/welcome.jpg?type=w773'
-                />
-              </Grid>
-            )}
+            <Grid
+              container
+              direction='column'
+              alignItems='center'
+              justifyContent='center'
+            >
+              <Box
+                component='img'
+                sx={{
+                  height: 600,
+                  width: 800,
+                }}
+                src='https://postfiles.pstatic.net/MjAyMjA2MDJfODMg/MDAxNjU0MTIyMjcwNzE3.2fThIQuTaZl2OOT3uba6gGl6gd-N_JCWGtpJXlqYDIAg.N-WoprcIWBYp_Q_BT1sC1uOEiwYw9EiqbSLIHNR3x-Ug.JPEG.hatbi2000/welcome.jpg?type=w773'
+              />
+            </Grid>
           </Stack>
         </Container>
       </main>
--- pages/recommendations.js
+++ pages/recommendations.js
@@ -1,8 +1,29 @@
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import { Container, Grid, Stack } from '@mui/material'
+import { useState, useContext, useEffect } from 'react'
+import { UserContext } from '../contexts/UserContext'
 
 export default function Recommendations() {
+  const { user } = useContext(UserContext)
+  const [recommendation, setRecommendation] = useState(null)
+  async function getRecs() {
+    const res = await fetch(
+      `http://localhost:3000/api/userrecs?username=${user.username}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((rec) => setRecommendation(Object.keys(rec)[0]))
+  }
+  useEffect(() => {
+    if (user) getRecs()
+  }, [user])
   return (
     <div>
       <Head>
@@ -11,7 +32,29 @@ export default function Recommendations() {
         <link rel='icon' href='/logo2.jpg' />
       </Head>
       <Navbar />
-      <main></main>
+      <main>
+        <Container>
+          <Stack spacing={3}>
+            <Container>
+              <Grid
+                container
+                direction='column'
+                alignItems='center'
+                justifyContent='center'
+              >
+                {/*<p>*/}
+                {/*  Click 'Get' and wait. I will let you know when the result is*/}
+                {/*  ready!*/}
+                {/*</p>*/}
+                {/*<Button variant='contained' type='submit' onClick={getRecs}>*/}
+                {/*  GET RECOMMENDATION*/}
+                {/*</Button>*/}
+                <p>Here is your recommendation: {recommendation}</p>
+              </Grid>
+            </Container>
+          </Stack>
+        </Container>
+      </main>
       <Footer />
     </div>
   )
